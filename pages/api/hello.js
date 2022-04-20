0866ed9b95032b6d98e3e9174be423dd2a593672
iknowhtml <iknowsmtp@gmail.com>
2022-04-19 21:57:48 -0400

    Add bug reproduction

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import CustomAuth from "@toruslabs/customauth";
+
+new CustomAuth({
+  baseUrl: "localhost:3000",
+});
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
