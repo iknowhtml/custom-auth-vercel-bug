5f7e8797aa877a1e813ebd312bb3c05e85b8ea5d
iknowhtml <iknowsmtp@gmail.com>
2022-04-19 21:55:36 -0400

    Add bug reproduction

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import CustomAuth from "@toruslabs/customauth";
+
+new CustomAuth({
+  baseUrl: "localhost:3000",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
